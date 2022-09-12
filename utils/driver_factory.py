a6bd461d6b4713ba6e83a6b38b582f894a9defa0
abhishekishere98 <84777190+abhishekishere98@users.noreply.github.com>
2022-09-12 19:48:27 +0530

    Commit 12th Sep - working - 1st

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from webdriver_manager.firefox import GeckoDriverManager
 
 
@@ -14,5 +15,20 @@ class DriverFactory:
         elif config.lower() == "firefox":
             options = webdriver.FirefoxOptions()
             driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
+            return driver
+        elif config.lower() == "chromium":
+            chrome_options = webdriver.ChromeOptions()
+            options = [
+                "--headless",
+                "--disable-gpu",
+                "--window-size=1920,1200",
+                "--ignore-certificate-errors",
+                "--disable-extensions",
+                "--no-sandbox",
+                "--disable-dev-shm-usage"
+            ]
+            for option in options:
+                chrome_options.add_argument(option)
+            driver = webdriver.Chrome(executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), options=chrome_options)
             return driver
         raise Exception("Please provide a valid \"driver\" name")
